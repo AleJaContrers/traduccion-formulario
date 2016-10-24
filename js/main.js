a5461391263a279a3a6951dec2ce9afbebf4c33f
AleJaContrers <aleja.contreras.h@gmail.com>
2016-10-24 13:46:59 -0300

    cambios en js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Cambiar Please sign in
+document.getElementById("form-signin-heading").innerHTML = "Por favor inicia sesión";
+
+// Cambiar Please enter your email
+var input = document.getElementById ("inputEmail");
+input.placeholder = "Correo Electrónico"
+
+// Cambiar Password
+var input = document.getElementById ("inputPassword");
+input.placeholder = "Contraseña"
+
+// Cambiar Remember me
+document.querySelector(".checkbox span").innerHTML="Recordar datos";
+
+// Cambiar Sign in
+document.getElementsByTagName('button')[0].innerHTML="Iniciar Sesión";
